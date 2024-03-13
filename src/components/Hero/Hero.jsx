1077e190fc03f05259cbd663632d529ec6da4709
mehrshin <writeshirin@gmail.com>
2024-03-13 17:18:35 -0400

    Update skills

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ export const Hero = () => {
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Shirin</h1>
-        <p className={styles.description}>Talented individual with strong team spirit, keen eye for details, and B.S. in Software Engineering. Offering outstanding ability to work with various programming languages, including Java , C#, Python, and PHP. Reach out if you'd like to learn more!</p>
+        <p className={styles.description}>Talented individual with strong team spirit, keen eye for details, and B.S. in Software Engineering. Offering outstanding ability to work with various programming languages, including Java, C#, Python, and PHP. Reach out if you'd like to learn more!</p>
         <a href="mailto: writeshirin@gmail.com" className={styles.contactBtn}>Contact Me</a>
       </div>
       <img src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" className={styles.heroImg}/>
