902cb53f05939a1406c987a52bee3f2fba077709
mehrshin <writeshirin@gmail.com>
2024-03-13 15:36:49 -0400

    Add existing project files to git

---
+++ src/components/About/About.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { getImageUrl } from "../../utils";
+import styles from "./About.module.css";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor Icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>I'm a frontend developer with experience in building responsive and optimized websites.</p>
+            </div>
+          </li>
+
+
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server Icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <p>I have experience developing fast and optimzied backend systems and APIs. </p>
+            </div>
+          </li>
+
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Wp Icon" />
+            <div className={styles.aboutItemText}>
+              <h3>WordPress Developer</h3>
+              <p>I'm a WordPress developer with a focus on creating visually appealing and user-friendly websites.</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { getImageUrl } from '../../utils';
+
+import styles from "./Contact.module.css";
+
+export const Contact = () => {
+  return (
+    <footer id='contact' className={styles.container}>
+        <div className={styles.text}><h2>Contact</h2>
+        <p>Feel free to reach out</p></div>
+        <ul className={styles.links}>
+            <li className={styles.link}>
+                <img src={getImageUrl("contact/emailIcon.png")} alt='Email Icon' />
+                <a href="mailto: writeshirin@gmail.com">writeshirin@gmail.com</a>
+            </li>
+            <li className={styles.link}>
+                <img src={getImageUrl("contact/linkedinIcon.png")} alt='Linkedin Icon' />
+                <a href="https://www.linkedin.com/in/shirinnasir/">linkedin.com/shirinnasir</a>
+            </li>
+            <li className={styles.link}>
+                <img src={getImageUrl("contact/githubIcon.png")} alt='Github Icon' />
+                <a href="https://github.com/Mehrshin">github.com/mehrshin</a>
+            </li>
+        </ul>
+    </footer>
+  )
+}
